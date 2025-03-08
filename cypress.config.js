5a326b0489f0ffd48bcad74bcaf8ff2899630f44
mariatj-qa <maria.tabakh.jashni@gmail.com>
2025-03-08 14:08:44 -0500

    added files

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    reporter: 'cypress-mochawesome-reporter',
+    reporter: 'mochawesome',
     e2e: {
         setupNodeEvents(on, config) {
-            require('cypress-mochawesome-reporter/plugin')(on);
         },
         env:{
             baseUrl: 'https://www.saucedemo.com/',
